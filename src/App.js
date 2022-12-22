dc647251ef068d848c1f96916280212edf651a2a
Rishav Sinha <rishav@live.co.uk>
2022-12-23 02:01:24 +0530

    Final Changes

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className='App-header'>
         <img src={logo} className='App-logo' alt='logo' />
         <div style={{ fontSize: 30 }}>
-          Version <span style={{ color: 'red' }}>3</span>
+          Version <span style={{ color: 'red' }}>WORKIGNGNGNGNGN</span>
         </div>
       </header>
     </div>
