604ac3b39a717bbceb7e0386debaed67fd7857bb
Rishav Sinha <rishav@live.co.uk>
2022-12-26 01:33:12 +0530

    Final Commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,8 @@ function App() {
       <header className='App-header'>
         <img src={logo} className='App-logo' alt='logo' />
         <div style={{ fontSize: 30 }}>
-          Version <span style={{ color: 'red' }}>Second Commit....!!!</span>
+          Version{' '}
+          <span style={{ color: 'red' }}>Third and FINAL Commit....!!!</span>
         </div>
       </header>
     </div>
