812760f8a3ad592e6a44dfe07f544feb27463b98
Rishav Sinha <rishav@live.co.uk>
2022-12-26 01:13:45 +0530

    Changes First Commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className='App-header'>
         <img src={logo} className='App-logo' alt='logo' />
         <div style={{ fontSize: 30 }}>
-          Version <span style={{ color: 'red' }}>WORKIGNGNGNGNGN</span>
+          Version <span style={{ color: 'red' }}>First Commit....!!!</span>
         </div>
       </header>
     </div>
