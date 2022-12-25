7a88f8062a4c0f9f1630500eeacd9b009b98101f
Rishav Sinha <rishav@live.co.uk>
2022-12-26 01:23:42 +0530

    2nd change

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className='App-header'>
         <img src={logo} className='App-logo' alt='logo' />
         <div style={{ fontSize: 30 }}>
-          Version <span style={{ color: 'red' }}>First Commit....!!!</span>
+          Version <span style={{ color: 'red' }}>Second Commit....!!!</span>
         </div>
       </header>
     </div>
